ca767990984aa9c84b9b2061b85b636ca0f31e57
basameera <basameera@users.noreply.github.com>
2017-05-16 20:38:06 +0530

    Add files via upload

---
+++ examples/bassRandom/bassRandom.ino
@@ -0,0 +1,14 @@
+#include <BASSLib.h>
+
+BASSLib bass;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  long num = bass.getRandomNumber();
+  Serial.println(num);
+  delay(1000);
+
+}
