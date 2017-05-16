7a779db9cc5d49c08b48a2d02276f0bd878e3b99
basameera <basameera@users.noreply.github.com>
2017-05-16 20:35:53 +0530

    Add files via upload

---
+++ BASSLib.cpp
@@ -0,0 +1,30 @@
+#include "BASSLib.h"
+
+BASSLib::BASSLib(bool displayMsg){
+  if(displayMsg){
+    
+  }
+}
+
+long BASSLib::getRandomNumber(){
+  unsigned long sp = random(5, 100);
+  return sp;
+}
+
+float BASSLib::CompFilter(float x){
+//filter co-efficients
+float Ca=CF_CE; //value between 0 & 1
+float Cb=(1-Ca);   
+  static float pre_x=1500, out=0;
+  out = (Ca * pre_x) + (Cb * x);
+  pre_x = out;
+  return out;
+}
+
+float BASSLib::setCF_CE(float ce){
+	CF_CE = ce;
+}
+
+float BASSLib::getCF_CE(){
+	return CF_CE;
+}
---
+++ BASSLib.h
@@ -0,0 +1,26 @@
+#ifndef BASS_LIB
+#define BASS_LIB
+
+#if (ARDUINO>=100)
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+class BASSLib{
+  public:
+    //constructor
+    BASSLib(bool displayMsg=false);
+    
+    //methods
+    long getRandomNumber();
+	float CompFilter(float x);
+	float setCF_CE(float ce);
+	float getCF_CE();
+	
+  private:
+	float CF_CE=0;
+};
+
+
+#endif
